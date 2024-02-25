6de0428e5878ca29f6b703ef2515b6a425e37a86
Maiza Rojas <158431420+maiza02@users.noreply.github.com>
2024-02-25 11:02:24 -0800

    Add files via upload
    Topic 4 Activity 4 Part 1 C

---
+++ topic4_1c/src/app/FilePlay.java
@@ -0,0 +1,87 @@
+package app;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
+/**
+ * The FilePlay class provides methods for copying the content of one file to another
+ * and handling file input/output operations using BufferedReader and BufferedWriter.
+ */
+public class FilePlay {
+
+	
+	  /**
+     * The main method of the FilePlay class.
+     *
+     * @param args The command-line arguments passed to the program.
+     */
+	public static void main(String[] args) {
+		// Call method to copy files 
+		copyFile("InFile.txt", "OutFile.txt");
+		
+		// Call method to read/write files
+		int err = FilePlay.copyFile("InUsers.txt", "OutUsers.txt");
+		
+		// Display copy file results
+		switch(err) {
+		 
+		case 0: 
+			System.out.println("File was copied successfully."); 
+			break;
+		case -1:
+			System.out.println("File could not be opened.");
+			break;
+		case -2:
+			System.out.println("File I/O error."); 
+			break; 
+		}
+    }
+
+	
+
+    /**
+     * Copies the content of one file to another, processing each line to extract information.
+     *
+     * @param inFile  The path of the input file.
+     * @param outFile The path of the output file.
+     * @return 0 if the file was copied successfully, -1 if the file could not be opened, -2 if there was a file I/O error.
+     */
+    private static int copyFile(String inFile, String outFile) {
+        // Input and output File Read/Writer declarations
+    	BufferedReader  in = null;
+    	BufferedWriter  out = null;
+
+        try {
+            // Create Reader and Writer 
+            in = new BufferedReader(new FileReader(inFile));
+            out = new BufferedWriter(new FileWriter(outFile));
+
+            // Loop to read all characters from FileReader and write to FileWriter 
+            String line;
+            while ((line = in.readLine()) != null) {
+               
+            	String[] tokens = line.split("\\|");
+            	out.write(String.format("Name is %s %s of age %s\n", tokens[0], tokens[1], tokens[2]));
+            }
+            return 0; // Success
+        } catch (IOException e) {
+            // Handle IOException (File I/O error)
+            return -2;
+        }finally {
+            try {
+                // Close FileReader and FileWriter in the finally block to ensure they are closed
+                if (in != null) {
+                    in.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace(); // Handle closing error if necessary
+            }
+        }
+    }
+}
